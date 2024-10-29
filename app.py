7f675e10b6b22def0f7765b1993099a3f9dcf6a1
Rémi Canillas <remi.canillas@dougs.fr>
2024-10-29 11:57:59 +0100

    first implementation

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+count = 0
+count_list = []
+
+
+@app.route("/cat-gif")
+def cat_gif():
+    global count_list
+    global count
+    with open("cat_urls.json", "r") as cat_file:
+        cat_data = json.load(cat_file)
+    print(cat_data)
+    url_list = cat_data["urls"]
+    if url_list[0]["type"] == "cat":
+        return json.dumps(url_list[0]["url"])
+    else:
+        return 500
